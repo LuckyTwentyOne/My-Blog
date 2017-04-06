82e78a059930c9ed100a719bdc0650cf165aed25
Viacheslav Butov <vbutov91@gmail.com>
2017-04-06 18:06:03 +0300

    All controllers created

---
+++ src/main/java/ua/kh/butov/blog/controller/page/ArticleController.java
@@ -0,0 +1,20 @@
+package ua.kh.butov.blog.controller.page;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ua.kh.butov.blog.controller.AbstractController;
+
+@WebServlet("/article")
+public class ArticleController extends AbstractController {
+	private static final long serialVersionUID = 7089871282763553056L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		forwardToPage("article.jsp", req, resp);
+	}
+}
---
+++ src/main/java/ua/kh/butov/blog/listener/ApplicationListener.java
@@ -0,0 +1,22 @@
+package ua.kh.butov.blog.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebListener
+public class ApplicationListener implements ServletContextListener {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationListener.class);
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		LOGGER.info("Application 'MyBlog' started");
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		LOGGER.info("Application 'MyBlog' destroyed");
+	}
+}
